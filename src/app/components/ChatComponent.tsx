cf6e54c2bd2f8c500dce6970d76fa7f8bec82b78
myNewGitAcc <newregwithemail@gmail.com>
2026-08-05 17:51:32 +0200

    chat component placeholder

---
+++ src/app/components/ChatComponent.tsx
@@ -0,0 +1,7 @@
+export function ChatComponent() {
+  return (
+    <div id="chat" className="min-h-0 overflow-y-auto">
+      <h1 className="text-heading">Chat with consultant</h1>
+    </div>
+  );
+}
