36677c7371f441088a9bcf7f6cdc72d2c0bea1c9
myNewGitAcc <newregwithemail@gmail.com>
2026-08-05 23:10:59 +0200

    rewrite with clsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Providers from '@/app/providers';
+import clsx from 'classnames';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './global.css';
@@ -22,7 +23,11 @@ export default function RootLayout({ children }: LayoutProps<'/'>) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={clsx(
+        geistSans.variable,
+        geistMono.variable,
+        'h-full antialiased',
+      )}
     >
       <body className="flex h-full min-h-screen flex-col items-center text-default">
         <Providers>{children}</Providers>
