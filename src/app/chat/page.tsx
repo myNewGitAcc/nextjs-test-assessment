36d8807de35be45ce8eba2608d0b8fef49a4d3f0
myNewGitAcc <newregwithemail@gmail.com>
2026-08-06 00:27:19 +0200

    use PanelWorkspace

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,4 +1,5 @@
 import { ChatComponent } from '@/app/components/ChatComponent';
+import { PanelWorkspace } from '@/app/components/PanelWorkspace';
 import MeetingsList from '@/app/components/MeetingsList';
 import { getQueryClient } from '@/lib/get-query-client';
 import { meetingsQueryOptions } from '@/queries/meetings';
@@ -12,15 +13,14 @@ export default async function ChatPage() {
   return (
     <main className="flex h-full min-h-0 w-full flex-1 flex-col p-6">
       <HydrationBoundary state={dehydrate(queryClient)}>
-        <div className="flex justify-center min-h-0 h-full w-full flex-1">
-          <MeetingsList />
-          <div
-            role="separator"
-            aria-orientation="vertical"
-            className="mx-4 w-px shrink-0 self-stretch bg-neutral-300"
-          />
-          <ChatComponent />
-        </div>
+        <PanelWorkspace>
+          <PanelWorkspace.Slide ariaLabel="Meetings">
+            <MeetingsList />
+          </PanelWorkspace.Slide>
+          <PanelWorkspace.Slide ariaLabel="Chat">
+            <ChatComponent />
+          </PanelWorkspace.Slide>
+        </PanelWorkspace>
       </HydrationBoundary>
     </main>
   );
